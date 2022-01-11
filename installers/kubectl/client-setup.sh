2964617bc8b5fa4155f61b452b97153fd1908ec3
zhl003 <hualongzhong@yunify.com>
2022-01-11 07:16:09 +0000

    fix pgo client default installation

--- installers/kubectl/client-setup.sh
+++ installers/kubectl/client-setup.sh
@@ -12,7 +12,7 @@
 #  limitations under the License.
 
 # This script should be run after the operator has been deployed
-VERSION=$(head -1 ../../VERSION)
+VERSION=v2.0
 PGO_OPERATOR_NAMESPACE="${PGO_OPERATOR_NAMESPACE:-pgo}"
 PGO_USER_ADMIN="${PGO_USER_ADMIN:-pgouser-admin}"
 PGO_CLIENT_VERSION="${PGO_CLIENT_VERSION:-${VERSION}}"
